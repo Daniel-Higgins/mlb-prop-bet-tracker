8ac38c276a1ef52b61b5e0316bef54b13abe48ed
Daniel-Higgins <dannyhiggs5@gmail.com>
2024-04-01 22:34:22 -0400

    vers

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from boto3.dynamodb.conditions import Attr
 import boto3
 
 app = Flask(__name__)
-app.config['VERSION_INFO'] = 'V1.0.6'
+app.config['VERSION_INFO'] = 'V1.0.8'
 app.secret_key = "_5#y2LF4Q8z$as!kz(9,d]/"  # Use the generated key here
 
 session = boto3.Session()
