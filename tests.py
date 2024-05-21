b229942a255f87f62855fcb8b96ccf121aaaff70
Daniel Higgins <dannyhiggs5@gmail.com>
2024-05-21 08:56:19 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -11,10 +11,6 @@ current_date = datetime.now()
 yesterday_date = current_date - timedelta(days=1)
 # Format the date as 'YYYY-MMM-DD'
 formatted_date = yesterday_date.strftime('%Y-%m-%d')
-oa_api_key = 'c80f14133d3748d3c465f41d78bf57e5'
 
 # Call the function
 # fix_uid_in_ht()
-#fix_password("nickvolz1@gmail.com","idiot_face")
-
-get_bet_data("Higgins")
